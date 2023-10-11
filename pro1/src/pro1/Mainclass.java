85c2d46d49b04dd0f5da3b43fa92e7592f49010f
DESKTOP-LT74JBH\Stefania <clontastefania2003@gmail.com>
2023-10-11 19:48:50 +0300

    lab2

--- pro1/src/pro1/Mainclass.java
+++ pro1/src/pro1/Mainclass.java
@@ -1,11 +1,32 @@
 package pro1;
 
+import java.util.Scanner;
+
 public class Mainclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		Scanner input = new Scanner(System.in);
+		int InputValue = input.nextInt();
+		
+		System.out.println("Valoarea citita de la tastatura este: " + InputValue);
 
+		int maxInt = Integer.MAX_VALUE;
+		/*	System.out.println(maxInt + 1);
+	
+		/*int minInt = Integer.MIN_VALUE;
+		System.out.println(minInt - 1);
+		
+		float maxFloat = Double.MAX_VALUE;
+		System.out.println(maxFloat * 3);
+*/		
+		try {
+		System.out.println( maxInt / InputValue);
+		}
+		catch( ArithmeticException exception) {
+			System.out.println("val gresita ");
+			InputValue = input.nextInt();
+		}
+		System.out.println("val noua" + InputValue);
 	}
 
 }
